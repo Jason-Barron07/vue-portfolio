8c779b4e9f9d5184d9f0ec6241467bc77a19dbb3
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-07-05 16:16:18 +0200

    update

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -20,5 +20,7 @@ export default {
   background: rgb(255,51,129);
 background: linear-gradient(90deg, rgba(255,51,129,1) 35%, rgba(255,103,0,1) 100%);
   margin-bottom: 30px;
+  border-radius:0.5em;
 }
+
 </style>

1013b8b9f9eb6be8eec479160bb9cc8ea163400f
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-07-07 10:09:08 +0200

    update

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,33 +1,20 @@
 <template>
-    <div class=" animate__animated animate__shakeY">
-        <!-- <div class="image">
-        <img src="https://i.postimg.cc/jj4yhQzm/ad1c85b2-d059-44cf-aa85-d0f517a0baca.jpg" alt="landing page picture" class="img-fluid d-block w-100">
-        </div>
-        <div class="display-2">
-            <h1>I am an aspiring software developer</h1>
-        </div> -->
-        <div id="carouselExample" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://i.postimg.cc/jj4yhQzm/ad1c85b2-d059-44cf-aa85-d0f517a0baca.jpg" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://i.postimg.cc/jj4yhQzm/ad1c85b2-d059-44cf-aa85-d0f517a0baca.jpg" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://i.postimg.cc/jj4yhQzm/ad1c85b2-d059-44cf-aa85-d0f517a0baca.jpg" class="d-block w-100" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+    <div class="landing-2">
+        <div class="landing">
+          <h1 class="animate__animated animate__fadeInDown animate__delay-1s 1s">
+            My name is Jason Barron
+          </h1>
+          <p class="animate__animated animate__fadeInDown animate__delay-2s 2s">
+            I am an Aspiring software Developer 
+           
+          </p>
+          <div class="animate__animated animate__bounceIn animate__delay-3s 3s">
+            <router-link class="Abtn" to="/about"
+              ><button class="Abtn">Read More</button></router-link
+            >
           </div>
-    </div>
+        </div>
+      </div>
 </template>
 
 <script>
@@ -38,5 +25,50 @@
 
 <style scoped>
 
+p {
+    font-size: x-large;
+    color:#fff;
+    background-color:#FF6700;
+  }
+  .Abtn:hover {
+    background-color:#FF6700 ;
+    border: 1px solid #FF3381;
+  }
+  h1 {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 32px;
+    color:#fff;
+    background-color:#FF3381;
+    border-radius:0.5em;
+  }
+  p {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px;
+    border-radius:0.5em;
+    
+    
+  }
+  .Abtn {
+    background-color:#FF3381 ;
+    border: #FF6700;
+    border-radius: 5px;
+    height: 3rem;
+    color:#fff;
+  }
+  .landing {
+    height: 61vh;
+    border-radius: 5px;
+    
+  }
+  .landing-2{
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
   
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,83 +1,21 @@
-<!-- <template>
-    <HomeImage/>
-  
-</template>
-
-<script>
-import HomeImage from '@/components/HomeComp.vue'
-
-export default {
-  components:{
-    HomeImage
-  }
-}
-</script> -->
 <template>
-  <div class="landing-2">
-    <div class="landing">
-      <h1 class="animate__animated animate__fadeInDown animate__delay-1s 1s">
-        My name is Jason Barron
-      </h1>
-      <p class="animate__animated animate__fadeInDown animate__delay-2s 2s">
-        I am an Aspiring software Developer 
-       
-      </p>
-      <div class="animate__animated animate__bounceIn animate__delay-3s 3s">
-        <router-link class="Abtn" to="/about"
-          ><button class="Abtn">Read More</button></router-link
-        >
-      </div>
-    </div>
+  <div>
+    <HomeComp/>
   </div>
 </template>
+
 <script>
-export default {};
+
+import HomeComp from '@/components/HomeComp.vue'
+  export default {
+    components:{
+      HomeComp
+    }
+  }
 </script>
+
 <style scoped>
-p {
-  font-size: x-large;
-  color:#fff;
-  background-color:#FF6700;
-}
-.Abtn:hover {
-  background-color:#FF6700 ;
-  border: 1px solid #FF3381;
-}
-h1 {
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 32px;
-  color:#fff;
-  background-color:#FF3381;
-  border-radius:0.5em;
-}
-p {
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 30px;
-  border-radius:0.5em;
-  
-  
-}
-.Abtn {
-  background-color:#FF3381 ;
-  border: #FF6700;
-  border-radius: 5px;
-  height: 3rem;
-  color:#fff;
-}
-.landing {
-  height: 61vh;
-  border-radius: 5px;
-  
-}
-.landing-2{
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-}
 
 </style>
+
+
